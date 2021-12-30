1f91c14656b202d8bf9f8db890111bdfb3fdbac9
Herve Chendjou <hchend2@uic.edu>
2021-12-30 16:08:58 -0600

    node class

--- StackList.cpp
+++ StackList.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+class Node {
+    public:
+        int data; //
+        Node* next; //
+
+        //by default
+        Node() {
+            data = 0;
+            next = NULL;
+        }
+        //else...
+        Node(int data, Node* next) {
+            this->data = data;
+            this->next = next;
+        }
+};
+
 int main() {
     cout << "Hello! StackList " << endl;
 }
