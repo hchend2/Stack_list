4ec48352e44c076b7d4044706703d872dab39b3a
Herve Chendjou <hchend2@uic.edu>
2021-12-30 16:26:20 -0600

    insert funct

--- StackList.cpp
+++ StackList.cpp
@@ -6,19 +6,44 @@ class Node {
     public:
         int data; //
         Node* next; //
+        //Node* top;
 
-        //by default
+        //by default...
         Node() {
             data = 0;
             next = NULL;
+            //top = NULL;
         }
-        //else...
+        //parametized constructor...
         Node(int data, Node* next) {
             this->data = data;
             this->next = next;
+            //this->top = NULL;
         }
 };
 
+Node* top; // holds the top of the stack...
+
+// function to insert a node into the stack...
+void insert(int data) {
+    Node* newNode = new Node();
+
+    // stack heap is full...
+    if (!newNode) {
+        cout << "stack is full" << endl; //(stack overflow)...
+        exit(1);
+    }
+
+    //stack heap is not full...
+    newNode->data = data; // add data to stack...
+
+
+    newNode->next = top;
+    top = newNode;
+
+}
+
 int main() {
+
     cout << "Hello! StackList " << endl;
 }
