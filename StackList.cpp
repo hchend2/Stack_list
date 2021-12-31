f65adc97e80663a78f88e6cfe999c9d96b973afa
Herve Chendjou <hchend2@uic.edu>
2021-12-30 21:35:00 -0600

    popTopNode funct

--- StackList.cpp
+++ StackList.cpp
@@ -5,35 +5,35 @@ using namespace std;
 class Node {
     public:
         int data; //
-        Node* next; //
-        //Node* top;
+        Node* prev; //
 
-        //by default...
+        // by default...
         Node() {
             data = 0;
-            next = NULL;
-            //top = NULL;
+            prev = NULL;
+            prev = NULL;
         }
         //parametized constructor...
-        Node(int data, Node* next) {
+        Node(int data) {
             this->data = data;
-            this->next = next;
-            //this->top = NULL;
+            this->prev = NULL;
         }
 };
 
 class myStack {
     Node* top; // holds the top of the stack...
+    int size; // size of the stack...
     public:
         // by default...
         myStack() {
             top = NULL;
         }
-
-        void insert(int);
+        void insert(int data);
         void printStack();
         void topNode();
         bool isEmpty();
+        void popTopNode();
+        int Size() const;
 };
 
 // function to insert a node into the stack...
@@ -46,11 +46,15 @@ void myStack::insert(int data) {
         exit(1);
     }
     //stack heap is not full...
-    newNode->data = data; // add data to stack...
-    newNode->next = top; // save top into newNode pointing to next node...
+    newNode->prev = top; // save top into newNode pointing to next node...
     top = newNode; // make newNode the top of stack...
+    newNode->data = data; // add data to stack...
+    size++;
 }
-
+// function to return the size of the stack...
+int myStack::Size() const {
+    return size;
+}
 // function to print out the stack...
 void myStack::printStack() {
 
@@ -63,9 +67,9 @@ void myStack::printStack() {
     // stack not empty...
     tmp = top;
     while (tmp != NULL) {
-        cout << tmp->data <<endl;
-        tmp = tmp->next;
-    }
+        cout << tmp->data << endl;
+        tmp = tmp->prev; 
+    } 
 }
 // check if stack is empty...
 bool myStack::isEmpty() {
@@ -80,14 +84,43 @@ void myStack::topNode() {
         exit(1);
     }
 }
+// remove the top node from stack...
+void myStack::popTopNode() {
+
+    Node* tmp;
+    // stack is empty...
+    if (top == NULL) {
+        cout << endl << "Stack underflow " << endl;
+        exit(1);
+    }
+    // stack not empty...
+    tmp = top; // save top node into tmp...
+    top = top->prev; // sava pointing to next into top...
+    // free memory of top node...
+    delete tmp;
+    size--; 
+}
 
 int main() {
+
     myStack stack;
     stack.insert(0);
     stack.insert(1);
     stack.insert(2);
-
+    stack.insert(3);
+    stack.insert(4);
     stack.printStack();
+
     stack.topNode();
+    cout << endl;
+    stack.popTopNode();
+    stack.popTopNode();
+    stack.popTopNode();
+
+    stack.topNode();
+    cout << endl;
+    cout << "check" << endl;
+    stack.printStack();
+    
     cout << "Hello! StackList " << endl;
 }
